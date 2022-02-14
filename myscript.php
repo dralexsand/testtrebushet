c1f3bebad5ed770d40019356148efbf363f57420
dralexsand <dr.alex.sand.111@gmail.com>
2022-02-14 13:41:30 +0300

    init app

--- myscript.php
+++ myscript.php
@@ -1,5 +1,6 @@
 <?php
 
+// Проверяем наличие аргументов
 if (sizeof($argv) < 2) {
     die("Не заданы аргументы командной строки\n");
 }
@@ -10,10 +11,11 @@ if (substr($fileXml, -3) !== "xml") {
     die("Неверный формат файла\n");
 }
 
+// Получаем содержимое файла
 $xml = simplexml_load_file($fileXml) or die("Ошибка: Невозможно создать объект");
 
 $cars = [];
-
+// Обходим файл в поисках значений узла modification с атрибутом name
 foreach ($xml->xpath('//modification') as $node) {
     $cars[] = $node['name'];
 }
